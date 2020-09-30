fcbe70b9a03bd211a0b71c9d47ef8017a2a84be3
arinzeokorji <arinzeokorji@yahoo.com>
2020-09-30 21:42:32 +0100

    [Refactor]: Change file names

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -1,5 +1,5 @@
-$('#header').load('/src/navbar/navbar.html');
-$('#footer').load('/src/footer/footer.html');
+$('#header').load('/src/navbar/index.html');
+$('#footer').load('/src/footer/index.html');
 
 // video slider control
 $('.video-slider').slick({
